3c7f118c8983bf5b38d56ee21cc341f2acb15969
Francisco <franjavi4111@gmail.com>
2022-03-13 00:13:31 -0500

    refactor/initial branch push, redux and env created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
 const reducer = (state, action) => {
   switch (action.type) {
+    case "SET-ERROR":
+      return {
+        ...state,
+        error: action.payload,
+      };
+
     default:
       return state;
   }
